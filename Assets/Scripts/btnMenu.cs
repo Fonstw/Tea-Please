618e1cba63ff2ba2081f29d3f0390cc11f62adab
Unknown <twistfons@live.nl>
2018-08-07 18:14:16 +0200

    Basic UI
    Needs "working stack", probs need to implement the tea their time properties first.
    How do I do that..? Hmm, too hot to think.

---
+++ Assets/Scripts/btnMenu.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class btnMenu : MonoBehaviour
+{
+    public void CloseGame()
+    {
+        // This... Well it closes the game
+        Application.Quit();
+    }
+}
