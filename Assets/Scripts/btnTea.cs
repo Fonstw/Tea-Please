8b4887cb4326801afb7812a8857e5f5db681f3d6
Unknown <twistfons@live.nl>
2018-08-09 13:24:11 +0200

    Basic UI functionality
    Now have the tea stack actually function like a stack

---
+++ Assets/Scripts/btnTea.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class btnTea : MonoBehaviour
+{
+    // The object added to the stack
+    public GameObject teaStatus;
+    // The object to hold the stack
+    public GameObject theStack;
+
+    public void AddTea()
+    {
+        // Instantiate the prefab as an in-game object
+        GameObject newItem = Instantiate(teaStatus, theStack.transform);
+
+        // For some reason it gets set to x=-1020 and stuff, let's just fix that!
+        newItem.GetComponent<RectTransform>().anchoredPosition = new Vector2(-67, -76);
+    }
+}
